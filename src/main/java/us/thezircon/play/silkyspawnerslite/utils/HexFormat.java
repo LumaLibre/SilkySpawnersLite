c7ff8087478792a6c3c49037267551d89352b691
Jsinco <jonah@jsinco.dev>
2026-06-02 21:23:07 -0400

    fix: hex format

--- src/main/java/us/thezircon/play/silkyspawnerslite/utils/HexFormat.java
+++ src/main/java/us/thezircon/play/silkyspawnerslite/utils/HexFormat.java
@@ -11,15 +11,11 @@ public class HexFormat {
     private static final Pattern pattern = Pattern.compile("&#[a-fA-F0-9]{6}");
 
     public static String format(String msg) {
-        String[] split = Bukkit.getBukkitVersion().split("-")[0].split("\\.");
-        String minorVer = split[1]; //For 1.10 will be "10"
-        if (Integer.parseInt(minorVer) >= 16) {
-            Matcher match = pattern.matcher(msg);
-            while (match.find()) {
-                String color = msg.substring(match.start(), match.end());
-                msg = msg.replace(color, ChatColor.of(color.replace("&#","#")) + "");
-                match = pattern.matcher(msg);
-            }
+        Matcher match = pattern.matcher(msg);
+        while (match.find()) {
+            String color = msg.substring(match.start(), match.end());
+            msg = msg.replace(color, ChatColor.of(color.replace("&#","#")) + "");
+            match = pattern.matcher(msg);
         }
         return ChatColor.translateAlternateColorCodes('&', msg);
     }
